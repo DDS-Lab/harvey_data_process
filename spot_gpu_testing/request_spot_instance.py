3b11b8ed3cb17619cb87504e32a10c6d2b61902a
aescay <andrew.escay@gmail.com>
2018-07-25 11:12:09 -0700

    Made request_spot_instances.py pep8 compliant

--- spot_gpu_testing/request_spot_instance.py
+++ spot_gpu_testing/request_spot_instance.py
@@ -1,4 +1,4 @@
-import time 
+import time
 import boto3
 
 session = boto3.Session(profile_name='DSSG2018-ddd-team')
@@ -8,52 +8,64 @@ client = session.client('ec2')
 # starting spot instances
 
 request = client.request_spot_instances(
-                SpotPrice= '0.02',
+                SpotPrice='0.02',
                 InstanceCount=1,
                 Type='persistent',
                 LaunchSpecification={
                     'ImageId': 'ami-e580c79d',
                     'InstanceType': 't2.micro',
-                    'SecurityGroupIds': [ "sg-01c4ea7e" ],
+                    'SecurityGroupIds': ["sg-01c4ea7e"],
                     'KeyName': 'dssg2018-ddd-keypair',
-                    'SubnetId':"subnet-972d2ddf" ,
+                    'SubnetId': "subnet-972d2ddf",
                 }
             )
 
 
 # getting the request description
 request_id = request['SpotInstanceRequests'][0]['SpotInstanceRequestId']
-request_description = client.describe_spot_instance_requests(SpotInstanceRequestIds=[request_id])
+request_description = client.describe_spot_instance_requests(
+    SpotInstanceRequestIds=[request_id]
+)
 
 time.sleep(10)
 
 
-# client.get_waiter(request_description['SpotInstanceRequests'][0]['Status']['Code']=='fulfilled')
+client.get_waiter(request_description['SpotInstanceRequests'][0]['Status']
+                  ['Code'] == 'fulfilled')
 
 
-# if request_description['SpotInstanceRequests'][0]['Status']['Code']=='fulfilled':
-#	print('fullfilled')
-#else:
-#	print('not fullfiled')
+# if request_description['SpotInstanceRequests'][0]['Status']['Code'] == \
+#         'fulfilled':
+#     print('fullfilled')
+# else:
+#     print('not fullfiled')
 
 
-# creating a list of instances associated with the request (maybe I need to update this in case the instances gets restarted)
-instance_ids = [print(item) for item in request_description['SpotInstanceRequests']]
+# creating a list of instances associated with the request
+# (maybe I need to update this in case the instances gets restarted)
+instance_ids = [print(item) for item in
+                request_description['SpotInstanceRequests']]
 print(instance_ids)
 
 
 # tag all instances
-client.create_tags(Resources = instance_ids,Tags = [{'Key':'Name','Value':'spot_gpu'}])
-client.create_tags(Resources = instance_ids,Tags = [{'Key':'Project','Value':'dssg2018-ddd'}])
-client.create_tags(Resources = instance_ids,Tags = [{'Key':'Email','Value':'vms16@uw.edu'}])
+client.create_tags(Resources=instance_ids, Tags=[
+    {'Key': 'Name', 'Value': 'spot_gpu'}])
+client.create_tags(Resources=instance_ids, Tags=[
+    {'Key': 'Project', 'Value': 'dssg2018-ddd'}])
+client.create_tags(Resources=instance_ids, Tags=[
+    {'Key': 'Email', 'Value': 'vms16@uw.edu'}])
 
 # cancel all instances & the request
 
 
 # getting the most up to date instance ids
-request_description = client.describe_spot_instance_requests(SpotInstanceRequestIds=[request_id])
-instance_ids = [item['InstanceId'] for item in request_description['SpotInstanceRequests']]
-instance_ids = [print(item) for item in request_description['SpotInstanceRequests']]
+request_description = client.describe_spot_instance_requests(
+    SpotInstanceRequestIds=[request_id])
+instance_ids = [item['InstanceId'] for item in request_description[
+    'SpotInstanceRequests']]
+instance_ids = [print(item) for item in request_description[
+    'SpotInstanceRequests']]
 
 
 # terminate instances
