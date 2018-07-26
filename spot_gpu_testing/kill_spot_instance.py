5bbcac315ec49135cec45bfc65b0172dbadab3b0
aescay <andrew.escay@gmail.com>
2018-07-25 17:47:44 -0700

    Clean spot_gpu_testing scripts

--- spot_gpu_testing/kill_spot_instance.py
+++ spot_gpu_testing/kill_spot_instance.py
@@ -5,22 +5,25 @@ import boto3
 
 session = boto3.Session(profile_name='DSSG2018-ddd-team')
 
-client = session.client('ec2')
+client = session.client('ec2', region_name='us-west-2')
 
 request_description = client.describe_spot_instance_requests()
 
-instance_ids = [item['InstanceId'] for item in request_description['SpotInstanceRequests']]
+instance_ids = [item['InstanceId'] for item in
+                request_description['SpotInstanceRequests']]
 
-request_ids = [item['SpotInstanceRequestId'] for item in request_description['SpotInstanceRequests']]
+request_ids = [item['SpotInstanceRequestId'] for item in
+               request_description['SpotInstanceRequests']]
 
 print(request_ids)
 
 print(instance_ids)
-client.terminate_instances(InstanceIds = instance_ids)
 
-client.cancel_spot_instance_requests(SpotInstanceRequestIds = request_ids)
+client.terminate_instances(InstanceIds=instance_ids)
 
-#for request in requests['SpotInstanceRequests']:
-#	print(request['InstanceId'])
-#	instance_ids = [item['InstanceId'] for item in request]
-#	print(instance_ids)
+client.cancel_spot_instance_requests(SpotInstanceRequestIds=request_ids)
+
+# for request in requests['SpotInstanceRequests']:
+# 	print(request['InstanceId'])
+# 	instance_ids = [item['InstanceId'] for item in request]
+# 	print(instance_ids)
--- spot_gpu_testing/request_spot_instance.py
+++ spot_gpu_testing/request_spot_instance.py
@@ -11,6 +11,7 @@ request = client.request_spot_instances(
                 SpotPrice='0.02',
                 InstanceCount=1,
                 Type='persistent',
+                InstanceInterruptionBehavior='stop',
                 LaunchSpecification={
                     'ImageId': 'ami-e580c79d',
                     'InstanceType': 't2.micro',
@@ -24,14 +25,15 @@ request = client.request_spot_instances(
 # getting the request description
 request_id = request['SpotInstanceRequests'][0]['SpotInstanceRequestId']
 request_description = client.describe_spot_instance_requests(
-    SpotInstanceRequestIds=[request_id]
-)
+    SpotInstanceRequestIds=[request_id])
 
-time.sleep(10)
+print(request_id)
+
+time.sleep(20)
 
 
-client.get_waiter(request_description['SpotInstanceRequests'][0]['Status']
-                  ['Code'] == 'fulfilled')
+# client.get_waiter(request_description['SpotInstanceRequests'][0]['Status']
+#                   ['Code'] == 'fulfilled')
 
 
 # if request_description['SpotInstanceRequests'][0]['Status']['Code'] == \
@@ -43,8 +45,8 @@ client.get_waiter(request_description['SpotInstanceRequests'][0]['Status']
 
 # creating a list of instances associated with the request
 # (maybe I need to update this in case the instances gets restarted)
-instance_ids = [print(item) for item in
-                request_description['SpotInstanceRequests']]
+instance_ids = [item['InstanceId'] for item in request_description[
+    'SpotInstanceRequests']]
 print(instance_ids)
 
 
@@ -59,17 +61,32 @@ client.create_tags(Resources=instance_ids, Tags=[
 # cancel all instances & the request
 
 
-# getting the most up to date instance ids
-request_description = client.describe_spot_instance_requests(
-    SpotInstanceRequestIds=[request_id])
-instance_ids = [item['InstanceId'] for item in request_description[
-    'SpotInstanceRequests']]
-instance_ids = [print(item) for item in request_description[
-    'SpotInstanceRequests']]
-
-
-# terminate instances
-client.terminate_instances(instance_ids)
-
-
-client.cancel_spot_instance_requests(request_id)
+# # getting the most up to date instance ids
+# request_description = client.describe_spot_instance_requests(
+#     SpotInstanceRequestIds=[request_id])
+# instance_ids = [item['InstanceId'] for item in request_description[
+#     'SpotInstanceRequests']]
+#
+# instance_ids
+#
+# # terminate instances
+#
+#
+# class TerminateInstance:
+#     def terminate_instances(self, request_id):
+#         response = client.terminate_instances(
+#             InstanceIds=[instance_ids])
+#         return response
+#
+#
+# TerminateInstance().terminate_instances(instance_ids)
+#
+#
+# class CancelRequest:
+#     def cancel_instance_request(self, request_id):
+#         response = client.cancel_spot_instance_requests(
+#             SpotInstanceRequestIds=[request_id])
+#         return response
+#
+#
+# CancelRequest().cancel_instance_request(request_id)
